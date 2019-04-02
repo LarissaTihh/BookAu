cfa8b76fe8821ca5402d5b82d2642630ec275379
user <user@student303-4>
2019-04-02 17:07:36 +0300

    Helloy

---
+++ src/BookAu/Author.java
@@ -0,0 +1,18 @@
+package BookAu;
+
+public class Author {
+   private String [] authors = {"Стефан Цвейг", "Мари-Анри Бейль Стендаль", "Мери Стюарт", "Виктор Гюго"};
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+   
+   
+    
+}
+    
+
